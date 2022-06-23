c2d2bb51df9ef51f202267f9e76d2087b1a276ba
riqvade <40538535+riqvade@users.noreply.github.com>
2022-06-23 12:11:29 +0600

    28 урок

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Collections.Generic;
 
 namespace Rocky.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDBContext _db;
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objList = _db.Category;
+
+            return View(objList);
         }
     }
 }
